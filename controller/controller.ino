df5d5c31d7543748e5b459cd25835e22f5c502f3
Akash Nayar <akashknayar5@gmail.com>
2026-01-19 14:21:21 -0500

    able to send controller data fast (within 3 ms)

---
+++ controller/controller.ino
@@ -0,0 +1,103 @@
+#define LED_PIN 7
+
+#define TIMEOUT_MILLIS 1000
+
+byte read_chars = 0;
+byte rem_chars = 0;
+char buf[1 << 8 + 1];
+
+uint64_t prev_millis = 0;
+uint64_t prev_full = 0;
+
+uint64_t total_full = 0;
+uint64_t num_full = 0;
+
+
+bool initialized = false;
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+void loop() {
+  uint64_t curr_millis = millis();
+  if (curr_millis - prev_millis > TIMEOUT_MILLIS) {
+    initialized = false;
+    rem_chars = 0;
+    read_chars = 0;
+  }
+
+  // if (Serial1.available() < rem_chars) {
+  //   return;
+  // }
+
+  if (Serial1.available() > 0) {
+    // Serial.print("in buf: ");
+    // Serial.println(Serial1.available());
+    prev_millis = curr_millis;
+
+    if (!initialized) {
+      Serial.println("this path");
+      if (Serial1.read() != 0) {
+        return;
+      }
+
+      Serial.println("got zero");
+
+      // Clear the buffer
+      while (Serial1.available()) {
+        Serial1.read();
+      }
+
+      initialized = true;
+      Serial1.write(1);
+      Serial.println("sent 1");
+      return;
+    }
+
+    // Serial.print("rem_chars=");
+    // Serial.println(rem_chars);
+
+    if (rem_chars == 0) {
+      rem_chars = Serial1.read();
+      // Serial.print("just read ");
+      // Serial.println(rem_chars);
+      read_chars = 0;
+      buf[rem_chars] = '\0';
+      // Serial.print("I need ");
+      // Serial.println(rem_chars);
+    }
+
+    while (Serial1.available() > 0 && rem_chars > 0) {
+      buf[read_chars++] = Serial1.read();
+      // Serial.print("happened, read ");
+      // Serial.println(static_cast<byte>(buf[read_chars - 1]));
+      rem_chars--;
+    }
+
+    // Full messages received!
+    if (rem_chars == 0) {
+      // Serial.print("took: ");
+      // Serial.print(curr_millis - prev_full);
+      // Serial.print("got: ");
+      // Serial.print(buf);
+      // Serial.println("");
+      if (prev_full != 0) {
+        total_full += curr_millis - prev_full;
+        num_full++;
+      }
+
+      if (num_full % 10 == 0) {
+        Serial.print("avging (ms): ");
+        Serial.println(total_full / num_full);
+      }
+
+      prev_full = curr_millis;
+    }
+  }
+}
---
+++ controller/main.py
@@ -0,0 +1,85 @@
+import pygame
+import serial
+import sys
+import time
+
+TICK_RATE = 30  # Hz
+PRECISION = 2
+
+PORT = '/dev/cu.BiRover'
+BAUD = 9600
+DELIM = ','
+
+pygame.init()
+pygame.joystick.init()
+
+clock = pygame.time.Clock()
+
+if pygame.joystick.get_count() == 0:
+    print("Please connect a controller.")
+    sys.exit(1)
+
+joystick = pygame.joystick.Joystick(0)
+
+running = True
+
+
+def send_message(ser, msg):
+    data = bytearray()
+    toks = msg.split(DELIM)
+    data.append(len(toks))
+    data.extend([int(x) for x in toks])
+
+    ser.write(data)
+
+
+def serialize_value(val):
+    # Convert float in the range of -1 to 1 (0.1 granularity) to a 1-byte int
+    # 0 -> 128
+
+    return int(min(1, max(-1, val)) * 100) + 128
+
+
+try:
+    with serial.Serial(PORT, BAUD, timeout=1) as ser:
+        print("Connected.")
+
+        ser.write(int(0).to_bytes(1, 'big'))
+        print("Sent zero")
+
+        while int.from_bytes(ser.read(1), 'big') != 1:
+            continue
+        print("Got one")
+
+        time.sleep(.25)
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.JOYDEVICEREMOVED:
+                    print("Controller disconnected.")
+                    running = False
+
+            steering = round(joystick.get_axis(0), PRECISION)
+            brake = round(joystick.get_axis(4), PRECISION)
+            throttle = round(joystick.get_axis(5), PRECISION)
+
+            steering_ser = serialize_value(steering)
+            brake_ser = serialize_value(brake)
+            throttle_ser = serialize_value(throttle)
+
+            print(
+                f"\rSteering: {steering}, Brake: {brake}, Throttle: {throttle} -> {steering_ser}, {brake_ser}, {throttle_ser}", end="")
+
+            msg = f"{steering_ser}{DELIM}{brake_ser}{DELIM}{throttle_ser}"
+            send_message(ser, msg)
+
+            clock.tick(TICK_RATE)
+
+except serial.SerialException as e:
+    print(f"[Error] {e}")
+except KeyboardInterrupt:
+    print("Stopping...")
+
+pygame.quit()
