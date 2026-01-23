86f82a127f2a5457f6dd5f823c304dc9bf2d31de
Akash Nayar <akashknayar5@gmail.com>
2026-01-23 15:38:11 -0500

    more clean

--- birover/main.py
+++ birover/main.py
@@ -1,9 +1,12 @@
 import pygame
 import serial
-import sys
 import time
 import os
 from pynput.keyboard import Key, Listener
+
+# Avoid pygame welcome message and window
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 # Bluetooth constants
 PORT = '/dev/cu.BiRover'
@@ -24,26 +27,17 @@ SHIFT_DEFAULT = 0
 SHIFT_SPIN = 5
 DELIM = ','
 
+# Control state
 keys = set()
-
-# Avoid pygame window
-os.environ["SDL_VIDEODRIVER"] = "dummy"
+prev_state = (float('inf'), float('inf'), float('inf'), float('inf'))
+prev_time = 0
+running = True
 
 pygame.init()
 pygame.joystick.init()
-
 clock = pygame.time.Clock()
-
-if pygame.joystick.get_count() == 0:
-    print("Please connect a controller.")
-    sys.exit(1)
-
-joystick = pygame.joystick.Joystick(0)
-
-running = True
-
-prev_state = (float('inf'), float('inf'), float('inf'), float('inf'))
-prev_time = 0
+joystick = pygame.joystick.Joystick(
+    0) if pygame.joystick.get_count() > 0 else None
 
 
 def get_char(key):
@@ -89,14 +83,13 @@ try:
                 keys.discard(c)
 
         listener = Listener(on_press=on_press, on_release=on_release)
-        listener.daemon = True # Quicker shutdown
+        listener.daemon = True  # Quicker shutdown
         listener.start()
 
         while running:
             steering, spinning, brake, throttle = 0.0, 0.0, -1.0, -1.0
             if len(keys) > 0:
                 if 'esc' in keys:
-                    print("Running is now false")
                     running = False
                 if 'w' in keys:
                     throttle += 2
@@ -110,31 +103,34 @@ try:
                     spinning = 2 * steering
                     steering = 0.0
 
-            else:
+            elif joystick is not None:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         running = False
                     if event.type == pygame.JOYDEVICEREMOVED:
-                        print("Controller disconnected.")
                         running = False
 
-                steering = round(apply_deadzone(joystick.get_axis(0)), PRECISION_DEFAULT)
-                spinning = round(round(apply_deadzone(joystick.get_axis(2)) * 3) / 3, PRECISION_SPIN)
+                steering = round(apply_deadzone(
+                    joystick.get_axis(0)), PRECISION_DEFAULT)
+                spinning = round(round(apply_deadzone(
+                    joystick.get_axis(2)) * 3) / 3, PRECISION_SPIN)
                 brake = round(joystick.get_axis(4), PRECISION_DEFAULT)
                 throttle = round(joystick.get_axis(5), PRECISION_DEFAULT)
 
-            steering_ser = serialize_value(steering, VALUES_DEFAULT, SHIFT_DEFAULT)
+            steering_ser = serialize_value(
+                steering, VALUES_DEFAULT, SHIFT_DEFAULT)
             spinning_ser = serialize_value(spinning, VALUES_SPIN, SHIFT_SPIN)
             steering_spinning_ser = steering_ser | spinning_ser
             brake_ser = serialize_value(brake, VALUES_DEFAULT, SHIFT_DEFAULT)
-            throttle_ser = serialize_value(throttle, VALUES_DEFAULT, SHIFT_DEFAULT)
+            throttle_ser = serialize_value(
+                throttle, VALUES_DEFAULT, SHIFT_DEFAULT)
 
             curr_time = time.time()
             timeout = curr_time - prev_time >= TIMEOUT
             if (steering, spinning, brake, throttle) != prev_state or timeout:
                 print(
-                    f"\rSteering: {steering}, Spinning: {spinning}, Brake: {brake}, Throttle: {throttle}"
-                    f" ->{steering_spinning_ser}, {brake_ser}, {throttle_ser}", end="")
+                    f"\rSteering: {steering}, Spinning: {spinning}, "
+                    f"Brake: {brake}, Throttle: {throttle}", end=' ')
 
                 msg = f"{steering_spinning_ser}{DELIM}{brake_ser}{DELIM}{throttle_ser}"
                 send_message(ser, msg)
@@ -146,3 +142,6 @@ try:
                 clock.tick(TICK_RATE)
 
         pygame.quit()
+
+except serial.SerialException as e:
+    print(f"Unable to connect to BiRover")
